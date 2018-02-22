d9c8157b5af2f625a02946cf46ea62fd2114ede9
marina <marina.shatinskaya@gmail.com>
2018-02-23 00:12:08 +0300

    solution 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+
+    var set = {"H":0,"Q":0,"D":0,"N":0,"P":0};
+    
+    if (currency > 10000) 
+      return  {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    if (currency <=0)
+      return {};
+     
+    set.H = (currency/50 >= 1) ? parseInt(currency/50) : 0;
+    set.Q = (currency%50/25 >= 1) ? parseInt(currency%50/25) : 0; 
+    set.D = (currency%50%25/10 >= 1) ? parseInt(currency%50%25/10) : 0;
+    set.N = (currency%50%25%10/5 >= 1) ? parseInt(currency%50%25%10/5) : 0; 
+    set.P = (currency%50%25%10%5 > 0) ? parseInt(currency%50%25%10%5) : 0;
+    
+    for (var k in set){
+      if (set[k] == 0)
+        delete set[k];
+    }
+        return set; 
+
 }
